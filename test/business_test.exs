dda4bb56480b4d58b9bda65e6c2fd7e9f260e35a
marc@roosoft.com <marc@roosoft.com>
2022-11-17 16:42:52 -0500

    test use block height to get the block's hash

--- test/business_test.exs
+++ test/business_test.exs
@@ -7,8 +7,9 @@ defmodule BitcoinCoreClient.BusinessTest do
 
   alias BitcoinCoreClient.Rpc
   alias BitcoinCoreClient.Business
+  alias BitcoinLib.Block
 
-  setup do
+  setup_all do
     defmock(HttpMock, for: BitcoinCoreClient.Rpc.Http)
 
     expect(HttpMock, :start, fn -> :ok end)
@@ -21,11 +22,12 @@ defmodule BitcoinCoreClient.BusinessTest do
       http_module: HttpMock
     }
 
-    %{settings: settings}
+    url = Rpc.Settings.to_url(settings)
+
+    %{settings: settings, url: url}
   end
 
-  test "use block height to get the block's hash", %{settings: settings} do
-    url = Rpc.Settings.to_url(settings)
+  test "use block height to get the block's hash", %{settings: settings, url: url} do
     body = Rpc.Body.create("getblockhash", [0])
 
     expect(HttpMock, :post!, fn ^url, ^body, [] ->
@@ -37,5 +39,21 @@ defmodule BitcoinCoreClient.BusinessTest do
     hash = Business.get_block_hash(height, settings)
 
     assert hash == "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+  end
+
+  test "get block by hash", %{settings: settings, url: url} do
+    block_hash = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+
+    body = Rpc.Body.create("getblock", [block_hash, 0])
+
+    expect(HttpMock, :post!, fn ^url, ^body, [] -> encoded_genesis_block() end)
+
+    block = Business.get_block_by_hash(block_hash, settings)
+
+    assert block == encoded_genesis_block() |> Binary.from_hex() |> Block.decode()
+  end
+
+  defp encoded_genesis_block() do
+    "0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c0101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000"
   end
 end
