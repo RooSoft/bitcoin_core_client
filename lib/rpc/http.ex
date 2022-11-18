dcbdbb011e03b3cbe1a9f6256f756386c626fdc7
marc@roosoft.com <marc@roosoft.com>
2022-11-18 10:28:44 -0500

    docs and typespecs for Rpc.Http

--- lib/rpc/http.ex
+++ lib/rpc/http.ex
@@ -1,11 +1,23 @@
 defmodule BitcoinCoreClient.Rpc.Http do
+  @moduledoc """
+  Wrapper around HTTPoison to allow testable RPC calls over HTTP
+  """
+
   @callback start() :: any()
   @callback post!(binary(), binary(), list()) :: any()
 
+  @doc """
+  Starts the HTTP client
+  """
+  @spec start() :: :ok
   def start() do
     HTTPoison.start()
   end
 
+  @doc """
+  Sends a POST request over HTTP and returns the parsed result
+  """
+  @spec post!(binary(), binary(), list()) :: any()
   def post!(url, post_body, headers) do
     HTTPoison.post!(url, post_body, headers)
     |> Map.get(:body)
