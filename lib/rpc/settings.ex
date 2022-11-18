a9a6e0f709850e00af8f753ed521e43904f04da6
marc@roosoft.com <marc@roosoft.com>
2022-11-18 10:33:32 -0500

    added doctests and typespecs for Rpc.Settings

--- lib/rpc/settings.ex
+++ lib/rpc/settings.ex
@@ -1,8 +1,23 @@
 defmodule BitcoinCoreClient.Rpc.Settings do
+  @moduledoc """
+  A simple struct containing ip, port, credentials and a reference to the module
+  to use for HTTP calls
+  """
+
   defstruct [:ip, :port, :username, :password, http_module: BitcoinCoreClient.Rpc.Http]
 
   alias BitcoinCoreClient.Rpc.Settings
 
+  @doc """
+  Converts the struct into an URL that can be used to get information over a Bitcoin
+  Core RPC endpoint
+
+  ## Examples
+      iex> %BitcoinCoreClient.Rpc.Settings{ip: "192.168.1.1", port: 8332, username: "me", password: "pwd"}
+      ...> |> BitcoinCoreClient.Rpc.Settings.to_url()
+      "http://me:pwd@192.168.1.1:8332"
+  """
+  @spec to_url(%Settings{}) :: binary()
   def to_url(%Settings{} = settings) do
     "http://#{settings.username}:#{settings.password}@#{settings.ip}:#{settings.port}"
   end
---
+++ test/rpc/settings_test.exs
@@ -0,0 +1,5 @@
+defmodule BitcoinCoreClient.Rpc.SettingsTest do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinCoreClient.Rpc.Settings
+end
