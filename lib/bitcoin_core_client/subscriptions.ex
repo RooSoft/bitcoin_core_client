db89a08d1509fa90bc78bc2f55abdc7ec3156ef1
marc@roosoft.com <marc@roosoft.com>
2022-11-21 13:44:26 -0500

    fully documented the Subscriptions module

--- lib/bitcoin_core_client/subscriptions.ex
+++ lib/bitcoin_core_client/subscriptions.ex
@@ -9,18 +9,34 @@ defmodule BitcoinCoreClient.Subscriptions do
 
   @server Subscriptions.Server
 
+  @doc """
+  Starts the subscriptions server
+  """
+  @spec start_link() :: {:ok, pid()} | :ignore | {:error, {:already_started, pid()} | term()}
   def start_link() do
     start_link(nil)
   end
 
+  @doc """
+  Starts the subscriptions server
+  """
+  @spec start_link(any()) :: {:ok, pid()} | :ignore | {:error, {:already_started, pid()} | term()}
   def start_link(_) do
     GenServer.start_link(@server, %Subscriptions{}, name: @server)
   end
 
+  @doc """
+  Returns a list of pids that subscribed to new bitcoin block messages
+  """
+  @spec get_blocks_subscriptions() :: list()
   def get_blocks_subscriptions() do
     GenServer.call(@server, {:get_block_subscriptions})
   end
 
+  @doc """
+  Subscribes the current process to receive bitcoin block messages
+  """
+  @spec subscribe_blocks() :: :ok
   def subscribe_blocks() do
     GenServer.cast(@server, {:subscribe_blocks, self()})
   end
